99c453335cfc0c8afc78e22b26a332bea3d655d7
sami <sami@Frank2>
2015-03-09 14:33:51 +0200

    bugfix in the setup (I stupidly used a bad ln syntax) + minor changes in .bash_completion

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 for i in $(ls -pGA1 . | grep -e"^[.]" | grep -v /); do
 	echo "link $i[y|n]?"
 	read doLink
 	if [ "y" = "$doLink" ]; then
 		echo "$HOME/$i" "$HOME/${i}_old" | xargs -pt mv
-		echo "$i" "$HOME/${i}" | xargs -pt ln -s
+		echo "$DIR/$i" "$HOME/${i}" | xargs -pt ln -s
 
 	fi
 done
